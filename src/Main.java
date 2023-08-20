bfc747f6553af7e0d5a9d3e98810026a785058aa
Austin Bolton <aibolt3@gmail.com>
2023-08-20 10:32:41 -0500

    part 2

--- src/Main.java
+++ src/Main.java
@@ -39,6 +39,37 @@ public class Main {
         cupcakeMenu.add(cupcake);
         cupcakeMenu.add(redVelvet);
         cupcakeMenu.add(chocolate);
+
+        ArrayList<Drink> drinkMenu = new ArrayList<Drink>();
+
+        Drink water = new Drink();
+        Soda soda = new Soda();
+        Milk milk = new Milk();
+
+        System.out.println("We are in the middle of creating a drink menu. We currently have three drinks on the menu but we need to decide on pricing.");
+        water.type();
+        System.out.println("How much would you like to charge for a bottle of water? (Input a numerical number taken to 2 decimal places)");
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        water.setPrice(price);
+
+        System.out.println("We are in the middle of creating a drink menu. We currently have three drinks on the menu but we need to decide on pricing.");
+        soda.type();
+        System.out.println("How much would you like to charge for a can of soda? (Input a numerical number taken to 2 decimal places)");
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        soda.setPrice(price);
+
+        System.out.println("We are in the middle of creating a drink menu. We currently have three drinks on the menu but we need to decide on pricing.");
+        milk.type();
+        System.out.println("How much would you like to charge for a glass of milk? (Input a numerical number taken to 2 decimal places)");
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        milk.setPrice(price);
+
+        drinkMenu.add(water);
+        drinkMenu.add(soda);
+        drinkMenu.add(milk);
     }
 }
 class Cupcake{
@@ -63,5 +94,33 @@ class RedVelvet extends Cupcake {
 class Chocolate extends Cupcake {
     public void type() {
         System.out.println("A chocolate based cupcake, with chocolate frosting.");
+    }
+}
+
+class Drink {
+    public double price;
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void type(){
+        System.out.println("A bottle of natural spring water.");
+    }
+}
+
+class Soda extends Drink {
+    public void type(){
+        System.out.println("A cold can of soda.");
+    }
+}
+
+class Milk extends Drink {
+    public void type(){
+        System.out.println("A nice refreshing glass of milk.");
     }
 }
